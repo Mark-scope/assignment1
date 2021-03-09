c2dd1e416d5052fd2dc08b53c9abeba21d5121ac
Mark-scope <agyemangmark1@gmail.com>
2021-03-09 11:34:30 +0000

    made changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import MyFirstComponent from './myfirstcomponent'
+import MySecondComponent from './mysecondcomponent'
+import MyThirdComponent from './mythirdcomponent'
+import MyFourthComponet from './myfourthcomponent'
+import MyFithComponent from './myfithcomponent'
+import MySixthComponent from './mysixthcomponent'
+import MySeventhComponent from './myseventhcomponent'
+import MyEightComponent from './myeightcomponent'
+import MyNinthComponent from './myninthcomponent'
+import MyTenthComponent from './mytenthcomponent'
 
 function App() {
   
   return (
-    <div className="App">
-      Hello
+    <div>
+      <MyFirstComponent />
+      <MySecondComponent />
+      <MyThirdComponent />
+      <MyFourthComponet />
+      <MyFithComponent />
+      <MySixthComponent />
+      <MySeventhComponent />
+      <MyEightComponent />
+      <MyNinthComponent />
+      <MyTenthComponent />
     </div>
   );
 }
+// 
 
 
 
